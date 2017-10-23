053db17517b40853fd8bee16a7bcdcfe2873e3c2
Alex Zwinge <alexzwinge@gmail.com>
2017-10-23 08:39:04 -0700

    all done

---
+++ TestGame/ContextMenu.cs
@@ -0,0 +1,38 @@
+﻿using Unbroken.LaunchBox.Plugins;
+using Unbroken.LaunchBox.Plugins.Data;
+
+namespace TestGame
+{
+    public class ContextMenu : IGameMenuItemPlugin
+    {
+        public bool SupportsMultipleGames => false;
+
+        public string Caption => "Test Game";
+
+        public System.Drawing.Image IconImage => null;
+
+        public bool ShowInLaunchBox => true;
+
+        public bool ShowInBigBox => false;
+
+        public bool GetIsValidForGame(IGame selectedGame)
+        {
+            return true;
+        }
+
+        public bool GetIsValidForGames(IGame[] selectedGames)
+        {
+            return false;
+        }
+
+        public void OnSelected(IGame selectedGame)
+        {
+            selectedGame.Play();
+        }
+
+        public void OnSelected(IGame[] selectedGames)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
